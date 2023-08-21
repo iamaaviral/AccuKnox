5679dc1005dcf91a03a89fdc1056a142ba652965
Aviral Srivastava <srivastavaaviral1@gmail.com>
2023-08-21 16:13:09 +0530

    POC assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import data from "./data.json";
+import CytoscapeComponent from "react-cytoscapejs";
 
 function App() {
+
+  const [selectedNodeData, setSelectedNodeData] = useState(null)
+
+  const setListeners = (cy) => {
+    cy.on("click", "node", (event) => {
+      event.target ? setSelectedNodeData(event.target.data()) : setSelectedNodeData(null);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appWrapper">
+      <CytoscapeComponent
+        cy={(cy) =>  {
+          cy.add(data)
+          setListeners(cy);
+        }}
+        className="cytospaceWrapper"
+        // zoom={1}
+        stylesheet={[
+          {
+            selector: "node",
+            style: {
+              backgroundColor: "#69e",
+              label: "data(id)",
+            },
+          },
+          {
+            selector: "edge",
+            style: {
+              width: 3,
+              color: "red",
+              lineColor: "#369",
+              targetArrowColor: "#369",
+              targetArrowShape: "triangle",
+              label: "data(label)",
+              fontSize: "14px",
+              color: "#777",
+            },
+          },
+        ]}
+      />
+            {selectedNodeData && <div className="nodeDataWrapper">
+              <h2>Selected node details</h2>
+              <span>Selected NodeId: {`${selectedNodeData.id}`}</span>
+              <br />
+              <span>People followed count: {`${selectedNodeData.peopleFollowedCount}`}</span>
+              <br />
+              <span>Total post: {`${selectedNodeData.totalPosts}`}</span>
+      </div>}
     </div>
   );
 }
